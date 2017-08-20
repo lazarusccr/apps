6b9d8d8debbed8cffce7b1d8f5c06b48a27a26d3
lainz <leandrodiazoe@gmail.com>
2017-08-20 16:46:09 -0300

    more apps

--- docs/apps.js
+++ docs/apps.js
@@ -12,5 +12,55 @@ var apps = [
         url: 'http://greenfishsoftware.org/gfie.php#apage',
         icon: 'greenfishiconeditor.png',
         tags: '2d, editor, paint, photo, png, jpg, icon'
+    },
+    {
+        name: 'Advanced Password Generator',
+        description: 'Lightweight password generator tool to protect your security with randomly generated password',
+        url: 'https://sourceforge.net/projects/apwg/',
+        icon: 'advancedpasswordgenerator.png',
+        tags: ''
+    },
+    {
+        name: 'Ancestromania',
+        description: 'Genealogy software that can import and export to Gedcom, to Geneanet and web site',
+        url: 'https://ancestromania.net/index.php?page=&lng=en',
+        icon: 'ancestromania.png',
+        tags: ''
+    },
+    {
+        name: 'Advanced Wget GUI (AWWG)',
+        description: 'Cross platform and multilanguage download manager',
+        url: 'https://sites.google.com/site/awggproject/',
+        icon: 'awwg.png',
+        tags: ''
+    },
+    {
+        name: 'Bilancio Facile',
+        description: 'Manage the family budget with ease',
+        url: 'http://www.magicdev.org/en/?noredirect=en_GB',
+        icon: 'bilanciofacile.png',
+        tags: 'money'
+    },
+    {
+        name: 'CudaText',
+        description: 'Cross platform text editor with syntax highlighting and code completion',
+        url: 'http://uvviewsoft.com/cudatext/',
+        icon: 'cudatext.png',
+        tags: 'code, development'
+    },
+    {
+        name: 'LazLock',
+        description: 'Password manager for Windows & GNU/Linux that encrypts your data with 128 bit AES encryption',
+        url: 'https://sourceforge.net/projects/lazlock/',
+        icon: 'lazlock.png',
+        tags: 'security'
     }
 ]
+
+apps.sort((a, b) => {
+    if (a.name > b.name)
+        return 1
+    if (a.name < b.name)
+        return -1
+    return 0
+})
