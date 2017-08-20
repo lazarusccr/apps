803dcba7e519fb4e0e571010aa3abd6265f299a4
lainz <leandrodiazoe@gmail.com>
2017-08-20 14:09:29 -0300

    added gfie pro

--- docs/apps.js
+++ docs/apps.js
@@ -5,5 +5,12 @@ var apps = [
         url: 'https://github.com/bgrabitmap/lazpaint',
         icon: 'lazpaint.png',
         tags: '3d, 2d, editor, paint, photo, png, jpg'
+    },
+    {
+        name: 'Greenfish Icon Editor Pro',
+        description: 'Powerful open source image editor for creating icons, cursors, animations and icon libraries',
+        url: 'http://greenfishsoftware.org/gfie.php#apage',
+        icon: 'greenfishiconeditor.png',
+        tags: '2d, editor, paint, photo, png, jpg, icon'
     }
 ]
